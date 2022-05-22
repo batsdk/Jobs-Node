d49318e47e827f257e7a12b0c1efcf106637c192
batsdk <shemilkaweesha@gmail.com>
2022-05-22 20:33:18 +0530

    Model Updates

--- models/Job.js
+++ models/Job.js
@@ -1,29 +1,26 @@
 const mongoose = require("mongoose");
 
-const JobSchema = new mongoose.Schema(
-  {
-    company: {
-      type: String,
-      required: [true, "Must provide a company name"],
-      maxLength: 55,
-    },
-    position: {
-      type: String,
-      required: [true, "Must provide a position"],
-      maxLength: 55,
-    },
-    status: {
-      type: String,
-      enum: ["interview", "declined", "pending"],
-      default: "pending",
-    },
-    createdBy: {
-      type: mongoose.Types.ObjectId,
-      ref: "User", // Which model are you referencing
-      required: [true, "Please provide a user"],
-    },
+const JobSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "Must provide a company name"],
+    maxLength: 55,
   },
-  { timestamps: true }
-);
+  position: {
+    type: String,
+    required: [true, "Must provide a position"],
+    maxLength: 55,
+  },
+  status: {
+    type: String,
+    enum: ["interview", "declined", "pending"],
+    default: "pending",
+  },
+  createdBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User", // Which model are you referencing
+    required: [true, "Please provide a user"],
+  },
+});
 
 module.exports = mongoose.model("Job", JobSchema);
